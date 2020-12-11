f30da4f276c21d67a21144e5800d750d63e77aba
marziehsobhani <75842302+marziehsobhani@users.noreply.github.com>
2020-12-11 21:45:45 +0330

    تمرین 2

---
+++ Exercise2.py
@@ -0,0 +1,12 @@
+print("******************************************************************************************")
+
+mySentence = input("Enter a Sentence or Word: ")
+str2 = ""
+
+for i in range(len(mySentence)):
+    if(i % 2 == 0):
+        str2 = str2 + mySentence[i]
+print("Original String :  ", mySentence)
+print("Final String :     ", str2)
+
+print(" **********Sobhani Bajgiran********** ")
