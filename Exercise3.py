0c69f31294a6a7a0f6da1bcf70f1393800164c20
marziehsobhani <75842302+marziehsobhani@users.noreply.github.com>
2020-12-11 21:46:04 +0330

    تمرین 3

---
+++ Exercise3.py
@@ -0,0 +1,10 @@
+while True:
+   
+
+    MySentence = input("Please enter a long sentence : ")
+    MySentence_Splited = MySentence.split(" ")
+    print("\nBe soorat List kalamate jomle vared shode : ",MySentence_Splited)
+    print("Words totall: ",len(MySentence_Splited))
+
+
+    print("  **********SobhaniBajgiran**********  ")
